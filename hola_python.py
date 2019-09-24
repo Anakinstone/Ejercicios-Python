108baa7ed9ef681141b05a420b09534d30e6636a
Andres Aguilar <anakinstone@hotmail.com>
2019-09-24 07:34:23 -0500

    primer commit

---
+++ hola_python.py
@@ -0,0 +1,44 @@
+"""
+nombre= "Andres"
+apellido = "Aguilar"
+edad = 19
+fullname = '{} {}'.format(nombre, apellido)
+mensaje = 'feliz cumpleaños número ' + str(edad) +'!.'
+print(fullname)
+print(mensaje)
+"""
+"""
+Ejercicio 1
+Nombre:Andres Aguilar
+Materia:Programacion visual
+"""
+nombre = "Andres"
+print("Hola " + str(nombre) + "," + "¿te gustaria aprender Python hoy?")
+print (nombre.lower())
+print(nombre.upper())
+print(nombre.title())
+numero = 117
+print("mi numero favorito es " + str(numero))
+"""
+Ejercicio 2. cuadrado de un numero
+Nombre:Andres
+Materia:Programacion visual
+"""
+lista = list(range(1,11))
+print(lista)
+for  i in range(0,len(lista)):
+    lista[i]= lista[i]*lista[i]
+
+print(lista)
+"""
+Un millon
+"""
+millon = list(range(1,1000001))
+print(millon)
+print(min(millon))
+print(max(millon))
+
+
+"""
+Fin del ejercicio
+"""
